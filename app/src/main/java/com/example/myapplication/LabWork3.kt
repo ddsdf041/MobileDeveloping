0800cdc888f4eb2aa9ae8a4e80b7a7ea65b4d214
wjwndi <sofialevina2005@gmail.com>
2024-09-15 16:41:50 +0300

    lab work 3 done

--- app/src/main/java/com/example/myapplication/LabWork3.kt
+++ app/src/main/java/com/example/myapplication/LabWork3.kt
@@ -1,5 +1,44 @@
 package com.example.myapplication
 
-fun main(){
+import kotlin.math.sqrt
 
+fun task1(){
+    var number:Int? = null
+    println(number)   //вывело null
+    number = 123
+    val text:String = number.toString()
+    println(text)   //вывело 123
+}
+
+fun getFullName(firstName: String?, lastName: String?): String{
+    return "${firstName?.trim() ?: "Unknown"} ${lastName?.trim() ?: "Unknown"}"
+}
+
+fun calculateSquareRoot(number:Double?): Double{
+    return sqrt(number!!)
+}
+
+fun getStringLength(obj:Any?): Int{
+    val str = obj as? String
+    return str?.length ?: -1
+}
+
+fun main(){
+    println("Задание 1")
+    task1()
+    println("=".repeat(30))
+
+    println("Задание 2")
+    println(getFullName(null,null))
+    println(getFullName("Софья","Лёвина"))
+    println("=".repeat(30))
+
+    println("Задание 3")
+//    println(calculateSquareRoot(null))
+    println(calculateSquareRoot(25.0))
+    println("=".repeat(30))
+
+    println("Задание 4")
+    println(getStringLength(null))
+    println(getStringLength("123456789"))
 }
