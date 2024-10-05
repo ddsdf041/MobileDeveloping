97ab686c9a74cd6587a68b447a065cb5be917dbb
wjwndi <sofialevina2005@gmail.com>
2024-10-05 14:49:16 +0300

    lab work 6: Прогноз погоды 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,100 +1,80 @@
 package com.example.myapplication
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.example.myapplication.scenes.MainScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import org.json.JSONObject
 
 
-const val API_KEY = "6c2a92c79a8c4513906174753240410"
+//const val API_KEY = "6c2a92c79a8c4513906174753240410"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> innerPadding
-                    Greeting("London", this)
-                }
+                MainScreen()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> innerPadding
+//                    Greeting("London", this)
+//                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, context: Context) {
-    val state = remember {
-        mutableStateOf("Unknown")
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier.fillMaxHeight(0.5f)
-            .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Temp in $name = ${state.value}"
-            )
-        }
-        Box(
-            modifier = Modifier.fillMaxHeight()
-            .fillMaxWidth(),
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Button(onClick = {
-                getResult(name, state, context)
-            },
-                modifier = Modifier.padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Refresh")
-            }
-        }
-    }
-}
+//@Composable
+//fun Greeting(name: String, context: Context) {
+//    val state = remember {
+//        mutableStateOf("Unknown")
+//    }
+//    Column(modifier = Modifier.fillMaxSize()) {
+//        Box(
+//            modifier = Modifier.fillMaxHeight(0.5f)
+//            .fillMaxWidth(),
+//            contentAlignment = Alignment.Center
+//        ) {
+//            Text(text = "Temp in $name = ${state.value}"
+//            )
+//        }
+//        Box(
+//            modifier = Modifier.fillMaxHeight()
+//            .fillMaxWidth(),
+//            contentAlignment = Alignment.BottomCenter
+//        ) {
+//            Button(onClick = {
+//                getResult(name, state, context)
+//            },
+//                modifier = Modifier.padding(5.dp)
+//                    .fillMaxWidth()
+//            ) {
+//                Text(text = "Refresh")
+//            }
+//        }
+//    }
+//}
 
 
-private fun getResult(city:String, state: MutableState<String>, context:Context){
-    val url = "https://api.weatherapi.com/v1/current.json" +
-            "?key=$API_KEY&" +
-            "q=$city" +
-            "&aqi=no"
-    val queue = Volley.newRequestQueue(context)
-    val stringRequest = StringRequest(
-        Request.Method.GET,
-        url,
-        {
-            response ->
-            state.value = response
-            val obj = JSONObject(response)
-            state.value = obj.getJSONObject("current").getString("temp_c")
-        },
-        {
-            error ->
-            Log.d("MyLog", "Error $error")
-        }
-    )
-    queue.add(stringRequest)
-}
+//private fun getResult(city:String, state: MutableState<String>, context:Context){
+//    val url = "https://api.weatherapi.com/v1/current.json" +
+//            "?key=$API_KEY&" +
+//            "q=$city" +
+//            "&aqi=no"
+//    val queue = Volley.newRequestQueue(context)
+//    val stringRequest = StringRequest(
+//        Request.Method.GET,
+//        url,
+//        {
+//            response ->
+//            state.value = response
+//            val obj = JSONObject(response)
+//            state.value = obj.getJSONObject("current").getString("temp_c")
+//        },
+//        {
+//            error ->
+//            Log.d("MyLog", "Error $error")
+//        }
+//    )
+//    queue.add(stringRequest)
+//}
